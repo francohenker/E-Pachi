be4c169da9171822a9c6c84881ebfe1c95b96dd0
Franco Henker <francohenker@gmail.com>
2024-10-03 23:59:23 -0300

    updates merge

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,25 +1,12 @@
-// import { Module } from '@nestjs/common';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
-// import { ProductoController } from './producto/producto.controller';
-// import { ProductoModule } from './producto/producto.module';
-
-// @Module({
-//   imports: [ProductoModule],
-//   controllers: [AppController, ProductoController],
-//   providers: [AppService],
-// })
-// export class AppModule {}
-
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Producto } from './producto/producto.entity';
 import { ProductoController } from './producto/producto.controller';
 import { ProductoModule } from './producto/producto.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Producto } from './producto/entities/producto.entity';
 import { UserModule } from './user/user.module';
 import { User } from './user/entities/user.entity';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
